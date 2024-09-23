4b86d28b2b7232ccd6555bdcb432552ba0062d81
Pavel <qwerty>
2024-09-23 19:55:02 +0300

    Add_Calculator

---
+++ src/Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator {
+
+    public static <T extends Number, U extends Number> double sum(T a, U b) {
+        return a.doubleValue() + b.doubleValue();
+    }
+
+    public static <T extends Number, U extends Number> double multiply(T a, U b) {
+        return a.doubleValue() * b.doubleValue();
+    }
+
+    public static <T extends Number, U extends Number> double divide(T a, U b) {
+        if (b.doubleValue() == 0) {
+            throw new ArithmeticException("Деление на ноль");
+        }
+        return a.doubleValue() / b.doubleValue();
+    }
+
+    public static <T extends Number, U extends Number> double subtract(T a, U b) {
+        return a.doubleValue() - b.doubleValue();
+    }
+}
