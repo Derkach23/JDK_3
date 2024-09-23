159e92f2ba91f934543ea23c15a834fd8811eadc
Pavel <qwerty>
2024-09-23 19:55:43 +0300

    Add_compareArrays

--- src/Utils.java
+++ src/Utils.java
@@ -1,2 +1,11 @@
+import java.util.Arrays;
+
 public class Utils {
+
+    public static <T> boolean compareArrays(T[] arr1, T[] arr2) {
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        return Arrays.equals(arr1, arr2);
+    }
 }
